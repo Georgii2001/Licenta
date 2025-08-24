69a1e0e5b540dac3ddca8d9982176ef4f589ffc6
florin.pintilie <florin.pintilie@itsmartsystems.eu>
2025-08-25 00:01:20 +0300

    add profile for accesing aws

--- spring-backend/Scripts/docker-image-build.sh
+++ spring-backend/Scripts/docker-image-build.sh
@@ -1,4 +1,4 @@
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $ECR
+aws ecr get-login-password --region eu-central-1 --profile jenkins | docker login --username AWS --password-stdin $ECR
 docker build -t $ECR/travelwithme-backend:"${VERSION}" .
 docker push $ECR/travelwithme-backend:"${VERSION}"
 docker rmi -f $ECR/travelwithme-backend:"${VERSION}"
