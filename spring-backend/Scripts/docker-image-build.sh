9ffbcabac8c72ed58543ac884825dcfc8252db8f
florin.pintilie <florin.pintilie@itsmartsystems.eu>
2025-08-24 22:06:20 +0300

    fix

--- spring-backend/Scripts/docker-image-build.sh
+++ spring-backend/Scripts/docker-image-build.sh
@@ -1,6 +1,5 @@
-cp target/travelwithme-backend-*.jar
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $ECR
-docker build -t $ECR/travelwithme-backend:"${VERSION}"
+docker build -t $ECR/travelwithme-backend:"${VERSION}" .
 docker push $ECR/travelwithme-backend:"${VERSION}"
 docker rmi -f $ECR/travelwithme-backend:"${VERSION}"
 rm target/*.jar
